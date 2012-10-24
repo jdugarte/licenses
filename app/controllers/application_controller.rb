bdd88a2402f124ef723c5b36bbb5ae80c97b5158
Jesus Dugarte <jdugarte@gmail.com>
2012-10-24 17:51:35 -0400

    add RecordNotFound message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
   protect_from_forgery
-  
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
   private
   
   def check_admin!
     redirect_to :root if !current_user.admin?
   end
   
+  def record_not_found(exception)
+    model = exception.message.match(/Couldn't find (.*) with.*/)[1].downcase
+    flash[:error] = "The #{model} you requested could not be found."
+    redirect_to url_for(model.classify.constantize)
+  end 
+    
 end
