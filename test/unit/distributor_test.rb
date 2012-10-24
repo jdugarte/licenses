3ea675794f2dd297c93a79cd4e6f514be73a87ae
Jesus Dugarte <jdugarte@gmail.com>
2012-10-24 15:15:28 -0400

    add unit tests

--- test/unit/distributor_test.rb
+++ test/unit/distributor_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class DistributorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not have empty name" do
+    dist = Distributor.new 
+    dist.distributor_id = 1
+    assert dist.invalid? 
+    assert dist.errors[:name].any?
+    assert !dist.save
+  end
+
+  test "must have a distributor parent" do
+    dist = Distributor.new :name => "Distributor"
+    assert dist.invalid?
+    assert !dist.save
+  end
+  
+  test "must have a distributor parent unless main?" do
+    dist = Distributor.new :name => "Distributor", :main => true
+    assert !dist.invalid?
+    assert dist.save
+  end
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not have empty name email" do
+    user = User.new 
+    user.distributor_id = 1
+    assert user.invalid? 
+    assert user.errors[:name].any?
+    assert user.errors[:email].any?
+    assert !user.save
+  end
+
+  test "must belong to a distributor" do
+    user = User.new :name => "User", :email => "email@local.com"
+    assert user.invalid?
+    assert !user.save
+  end
+  
 end
