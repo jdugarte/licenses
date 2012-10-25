e5c580dadd8c059f100860b7c79a3b1b2d1272fa
Jesus Dugarte <jdugarte@gmail.com>
2012-10-25 16:32:15 -0400

    add PCGuard lib

---
+++ lib/PCGuard.rb
@@ -0,0 +1,88 @@
+require 'win32ole'
+
+class PCGuard
+  
+  attr_reader :activation_code, :removal_code, :hd_volumen_serial, 
+              :motherboard_bios, :cpu, :hard_drive, :operating_system, :cd_dvd, 
+              :network_card
+  attr_accessor :programid, :sitecode, :mid, :features
+  
+  # "ResponseCode" values from PCGuard
+  STATUS_OK = 0             # Success
+  STATUS_BAD_SITE_CODE = 1  # Site code is invalid
+  STATUS_BAD_PROGRAM_ID = 3 # ProgramID is invalid
+  STATUS_BAD_MID_CODE = 6   # Machine ID is invalid
+  STATUS_MID_CODE_ERROR = 7 # Machine ID decryption error
+  
+  # Exceptions
+  class BadSiteCode < StandardError; end  
+  class BadProgramID < StandardError; end  
+  class BadMID < StandardError; end  
+  class MIDCodeError < StandardError; end  
+  
+  def initialize(programid = "", sitecode = "", mid = "", features = 0)
+
+    @programid = programid
+    @sitecode  = sitecode
+    @mid       = mid
+    @features  = features
+
+    @activation_code   = ""
+    @removal_code      = ""
+    @hd_volumen_serial = 0
+    @motherboard_bios  = 0 
+    @cpu               = 0
+    @hard_drive        = 0
+    @operating_system  = 0
+    @cd_dvd            = 0
+    @network_card      = 0
+    
+    self.generate unless (@programid.empty? || @sitecode.empty? || @mid.empty?)
+    
+  end
+  
+  def generate
+    
+    raise ArgumentError if (@programid.empty? || @sitecode.empty? || @mid.empty?)
+    
+    l = WIN32OLE.new("icgweb.icgen")
+    
+    # Validate machine codes
+    l.ProgramID = @programid
+    l.SiteCode  = @sitecode
+    l.MachineID = @mid
+    l.V5_CheckMachineID()
+    unless l.ResponseCode == STATUS_OK
+      raise BadSiteCode if l.ResponseCode == STATUS_BAD_SITE_CODE
+      raise BadMID if l.ResponseCode == STATUS_BAD_MID_CODE
+      raise MIDCodeError if l.ResponseCode == STATUS_MID_CODE_ERROR
+    end
+    
+    # Generate codes
+    l.SiteCode  = @sitecode
+    l.MachineID = @mid
+    l.Features  = @features
+    l.V5_CalculateActivationCode()
+    unless l.ResponseCode == STATUS_OK
+      raise BadSiteCode if l.ResponseCode == STATUS_BAD_SITE_CODE
+      raise BadProgramID if l.ResponseCode == STATUS_BAD_PROGRAM_ID
+      raise BadMID if l.ResponseCode == STATUS_BAD_MID_CODE
+    end
+    
+    # Resulting codes and IDs
+    @activation_code   = l.ActivationCode
+    @removal_code      = l.RemoveCode
+    @hd_volumen_serial = l.DriveID.to_i
+    @motherboard_bios  = l.BiosID.to_i
+    @cpu               = l.CPUID.to_i
+    @hard_drive        = l.HDHWID.to_i
+    @operating_system  = l.OSID.to_i
+    @cd_dvd            = l.CDHWID.to_i
+    @network_card      = l.NETID.to_i
+    
+    # Return activation code
+    @activacion_code
+    
+  end
+  
+end
