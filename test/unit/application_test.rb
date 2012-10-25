2b0939d540156f05a00b963bdc387e63549fc357
Jesus Dugarte <jdugarte@gmail.com>
2012-10-25 11:22:53 -0400

    add unique names test for applications

--- test/unit/application_test.rb
+++ test/unit/application_test.rb
@@ -10,4 +10,17 @@ class ApplicationTest < ActiveSupport::TestCase
     assert !application.save
   end
 
+  test "should have unique name" do
+    application1 = Application.create(get_new_application)
+    assert application1.valid?
+    application2 = Application.new(get_new_application)
+    assert application2.invalid?, "invalid?"
+    assert application2.errors[:name].any?, "name"
+    assert !application2.save
+  end
+  
+  def get_new_application
+    { name: 'New application', ProgramID: '7F8FOIFKRI48RUJFNFIFI4RU', price: 9.99 }
+  end
+  
 end
